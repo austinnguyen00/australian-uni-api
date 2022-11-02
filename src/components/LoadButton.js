cad362a8de08f1403649fa2e515029fc628299e7
Austin <ndkhiem3010@gmail.com>
2022-11-02 11:27:12 +0800

    Adding LOAD button

--- src/components/LoadButton.js
+++ src/components/LoadButton.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
-const LoadButton = () => {
-  return <Button variant='contained'>LOAD</Button>;
+const LoadButton = ({ setUniversities }) => {
+  // Function to fetching uni API
+  const fetchingUni = async () => {
+    const response = await fetch(
+      'http://universities.hipolabs.com/search?country=Australia'
+    );
+    const data = await response.json();
+    setUniversities(data);
+  };
+  return (
+    <Button variant='contained' onClick={fetchingUni}>
+      LOAD
+    </Button>
+  );
 };
 
 export default LoadButton;
--- src/components/UniPage.js
+++ src/components/UniPage.js
@@ -1,26 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import UniTable from './UniTable';
 import LoadButton from './LoadButton';
 
 const UniPage = () => {
   const [universities, setUniversities] = useState([]);
 
-  useEffect(() => {
-    const fetchingUni = async () => {
-      const response = await fetch(
-        'http://universities.hipolabs.com/search?country=Australia'
-      );
-      const data = await response.json();
-      setUniversities(data);
-    };
-
-    fetchingUni();
-  }, []);
-
   return (
     <>
       <h1>Australia Universities</h1>
-      <LoadButton></LoadButton>
+      <LoadButton setUniversities={setUniversities}></LoadButton>
       <UniTable universities={universities}></UniTable>
     </>
   );
